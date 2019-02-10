e1cf275780c6a7ebb504594b4de9b3f3c0132233
Ricardo Falasca <ricardo@falasca.com.br>
2019-02-10 10:07:06 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deeppages',
-    version='0.2.0',
+    version='0.2.1',
     description="Django's database stored web content processor",
     long_description=open('README.md').read(),
     url='https://github.com/ricardofalasca/deep-pages',
